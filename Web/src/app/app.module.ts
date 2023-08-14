8c93ff8627980561748798fd6c3c45e7b9c81bcb
João_Gregorini <75985123+jvg21@users.noreply.github.com>
2023-08-14 18:42:40 -0300

    cards

--- Web/src/app/app.module.ts
+++ Web/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,MatToolbarModule, MatButtonModule, MatIconModule,MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
