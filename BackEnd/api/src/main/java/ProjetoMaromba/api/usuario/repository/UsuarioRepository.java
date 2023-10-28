ba7822a1f8cf571c68ff0f45a19afcd1890ad45f
João Gregorini <75985123+jvg21@users.noreply.github.com>
2023-10-28 16:26:17 -0300

    api teste 1

--- BackEnd/api/src/main/java/ProjetoMaromba/api/usuario/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package ProjetoMaromba.api.usuario.repository;
-
-import ProjetoMaromba.api.usuario.entity.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
-}
