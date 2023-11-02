2f431aa3a7ca27ba19394f1ce421b5216bdab60a
João Gregorini <75985123+jvg21@users.noreply.github.com>
2023-11-02 20:07:56 -0300

    rotas reestabelecidas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ErrorPageComponent } from './Components/Pages/error-page/error-page.component';
-import { ProdutoListComponent } from './Components/Pages/Produto/produto-list/produto-list.component';
+import { LayoutComponent } from './Components/Pages/Layout/layout/layout.component';
 import { HomePageComponent } from './Components/Pages/home-page/home-page.component';
 import { LoginComponent } from './Components/Pages/User/login/login.component';
-import { LayoutComponent } from './Components/Pages/Layout/layout/layout.component';
+import { ProdutoListComponent } from './Components/Pages/Produto/produto-list/produto-list.component';
+import { ErrorPageComponent } from './Components/Pages/error-page/error-page.component';
+import { FooterComponent } from './Components/Pages/Layout/footer/footer.component';
 
 const routes: Routes = [
-  {
-    path:'',component:LayoutComponent, children: [
-      {path:'',component:HomePageComponent},
-      {path:'home',component:HomePageComponent},
-      {path:'login',component:LoginComponent},
-      {path:'/produtoslist',component:ProdutoListComponent}
-    ]
-  },
+  {path:"",redirectTo:"navegar",pathMatch: 'full'},
+  {path:"navegar",component:LayoutComponent,children:[
+    {path:"",component:HomePageComponent}
+  ]},
+  {path:"adm",redirectTo:"",pathMatch:'full'},
   {path:'**' , pathMatch:'full', component:ErrorPageComponent}
-
-];
+  
+ ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
