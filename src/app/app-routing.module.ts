f88aac326cc4ee3f2d26e36f5310865aad7958c3
João Gregorini <75985123+jvg21@users.noreply.github.com>
2023-11-02 18:49:02 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './Components/Pages/error-page/error-page.component';
+import { ProdutoListComponent } from './Components/Pages/Produto/produto-list/produto-list.component';
+import { HomePageComponent } from './Components/Pages/home-page/home-page.component';
+import { LoginComponent } from './Components/Pages/User/login/login.component';
+import { LayoutComponent } from './Components/Pages/Layout/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:LayoutComponent, children: [
+      {path:'',component:HomePageComponent},
+      {path:'home',component:HomePageComponent},
+      {path:'login',component:LoginComponent},
+      {path:'/produtoslist',component:ProdutoListComponent}
+    ]
+  },
+  {path:'**' , pathMatch:'full', component:ErrorPageComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
