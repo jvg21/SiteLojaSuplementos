8f28be836ac15d264be41ed14555506161269f4b
João Gregorini <75985123+jvg21@users.noreply.github.com>
2023-11-02 23:03:29 -0300

    commit

---
+++ src/app/Application/model/produto.model.ts
@@ -0,0 +1,10 @@
+export class ProdutoModel{
+    private id?:number;
+    private nome?:string;
+    private valor?:number;
+    private marca?:string;
+    private peso?:string;
+
+
+}
+
--- src/app/Components/Pages/Produto/produto-list/produto-list.component.ts
+++ src/app/Components/Pages/Produto/produto-list/produto-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProdutoModel } from 'src/app/Application/model/produto.model';
+import { ProdutoService } from 'src/app/Application/service/produto.service';
 
 @Component({
   selector: 'app-produto-list',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./produto-list.component.css']
 })
 export class ProdutoListComponent {
+  displayedColumns: string[] = ['id','nome','valor','marca','peso'];
+  dataSource: ProdutoModel[] = [];
 
+  constructor(private produtoService: ProdutoService) {}
+  
+  ngOnInit(): void {
+    this.listar();
+  }
+
+  listar() {
+    this.produtoService.listar().subscribe({
+      next: (produto) => this.dataSource = produto,
+      error: (e) => console.error(e),
+      complete: () => console.info('complete') 
+  });
+  }
 }
