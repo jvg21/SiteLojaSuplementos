d8046c7c25f06d6b7e79f9d7730c4d9b4e27e227
João Gregorini <108041441+BrunoGuttervill@users.noreply.github.com>
2023-11-10 23:10:14 -0300

    teste

--- BackEnd/tecweb2023-back-main/src/main/java/br/pucpr/exemplo/usuario/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package br.pucpr.exemplo.usuario.repository;
-
-import br.pucpr.exemplo.usuario.model.entity.*;
-import org.springframework.data.jpa.repository.*;
-
-public interface UsuarioRepository extends JpaRepository<Usuario,Integer> {
-}
--- src/app/Application/model/transportadora.model.ts
+++
@@ -1,8 +0,0 @@
-export class TransportadoraModel {
-    id?:number;
-    cnpj?:string;
-    nome?:string;
-    email?:string;
-    ativado?:boolean;
-
-}
