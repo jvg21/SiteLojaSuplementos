092e0916ab30cb911c3b493f164264bdb1dcdd38
João Gregorini <108088337+RibeiroEtk@users.noreply.github.com>
2023-11-11 15:59:31 -0300

    sd

--- BackEnd/tecweb2023-back-main/src/main/java/br/pucpr/exemplo/produto/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.pucpr.exemplo.produto.repository;
-
-import br.pucpr.exemplo.produto.model.entity.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto,Integer> {
-}
--- BackEnd/tecweb2023-back-main/src/main/java/br/pucpr/exemplo/transportadora/repository/TransportadoraRepository.java
+++
@@ -1,7 +0,0 @@
-package br.pucpr.exemplo.transportadora.repository;
-
-import br.pucpr.exemplo.transportadora.model.entity.Transportadora;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransportadoraRepository extends JpaRepository<Transportadora,Integer> {
-}
