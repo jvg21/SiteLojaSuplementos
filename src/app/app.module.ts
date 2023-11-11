f506f31e99fc01845da8a349c5db68c01edce909
João Gregorini <joaovgregorini@gmail.com>
2023-11-10 22:33:52 -0300

    pedido remove

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,8 +32,6 @@ import { TransportadoraListComponent } from './Components/Pages/Transportadora/t
 import { CarrinhoComponent } from './Components/Pages/carrinho/carrinho.component';
 import { CardComponent } from './Components/card/card.component';
 import { ProdutoSearchComponent } from './Components/Pages/Produto/produto-search/produto-search.component';
-import { PedidosComponent } from './Components/Pages/Produtos/pedidos/pedidos.component';
-import { PedidoComponent } from './Components/Pages/Produto/pedido/pedido.component';
 
 @NgModule({
   declarations: [
@@ -51,8 +49,6 @@ import { PedidoComponent } from './Components/Pages/Produto/pedido/pedido.compon
     CarrinhoComponent,
     CardComponent,
     ProdutoSearchComponent,
-    PedidosComponent,
-    PedidoComponent,
 
   ],
   imports: [
