3f55360c222edae5778aba2b6d41820b32a6b182
jvg21 <joaovitorgregorini@gmail.com>
2023-11-16 13:36:54 -0300

    pedidolist1

--- src/app/Components/Pages/Pedido/pedido-list/pedido-list.component.ts
+++ src/app/Components/Pages/Pedido/pedido-list/pedido-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { PedidoModel } from 'src/app/Application/model/pedido.model';
+import { PedidoService } from 'src/app/Application/service/pedido.service';
 
 @Component({
   selector: 'app-pedido-list',
@@ -6,5 +9,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./pedido-list.component.css']
 })
 export class PedidoListComponent {
+  displayedColumns: string[] = ['id', 'nome', 'valor', 'marca', 'peso','medidaPeso','ativado','descricao','url'];
+  dataSource: PedidoModel[] = [];
+  selectedRow = new PedidoModel;
+  listaPedidos: PedidoModel[] = [];
+  constructor(private pedidoService: PedidoService) { }
 
+  ngOnInit(): void {
+    this.listar();
+  }
+
+  logs(row: PedidoModel) {
+    this.selectedRow = row;
+
+  }
+  
+
+  listar() {
+
+    ////listar todos os pedidos
+    this.pedidoService.listar().subscribe({
+      next: (pedido) => this.listaPedidos = pedido,
+      error: (e) => console.error(e),
+      complete: () => console.info('complete')
+    });
+  }
+
+  alterar(){
+    ///alterar o status
+
+    let pedido = new PedidoModel;
+    pedido.id = Number(this.produtoForm.controls['Id'].value);
+    pedido.nome = this.produtoForm.controls['Nome'].value || '';
+    
+    this.pedidoService.alterar(pedido).subscribe(produto => {
+      this.listar()
+      alert(produto.nome + " Alterado Com sucesso");
+    });
+    
+  }
 }
